4d1c3a53d0aa83409d3f2c75f69b57adad8725dd
Joe Finney <joe@comp.lancs.ac.uk>
2024-02-28 17:50:29 +0000

    BREAK THE BUILDgit add Main.java !

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
     {
         GameArena arena = new GameArena(500,400);
 
-        StarField f = new StarField(300, 3, arena);
+        StarField f = new StarField(300, 3, arena)
         BigSpaceShip ship = new BigSpaceShip(100, 100, arena, 10);
 
         while(true)
